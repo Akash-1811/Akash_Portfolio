b153705afc1ba61b7d1cec51ee74e2bda4f4f691
AkYadav-18 <akash.yadavv181198@gmail.com>
2025-09-02 22:36:17 +0530

    Map Domain

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/Akash_Portfolio">
+        <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
